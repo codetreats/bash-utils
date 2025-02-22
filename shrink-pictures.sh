2aee07412c1449121dd1083a55ca6feaa9f375e4
Martin <github@msturm.net>
2025-02-22 14:51:41 +0100

    fix: replace jpg with png in shrink pictures

--- shrink-pictures.sh
+++ shrink-pictures.sh
@@ -15,7 +15,7 @@ find . -type d | while read DIR; do
 	for FILE in $(find .  -maxdepth 1 -type f -iname "*.png"); do
 		FILE=$(echo $FILE | cut -d "/" -f2)
 		echo convert $FILE
-  		convert "$FILE" -resize 1200x1200 "$FILE.res.png"
+  		convert "$FILE" -resize 1200x1200 "$FILE.res.jpg"
 	done
 done
 
